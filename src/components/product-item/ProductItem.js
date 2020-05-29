d6daa08213ab39fbee0c0b48dfd8c0c1d805a8bf
João Vitor <joao.vitor@onblox.com.br>
2020-05-29 13:05:48 -0300

    Modifica componente product-item. Alteração enviada para mudança de máquina

--- src/components/product-item/ProductItem.js
+++ src/components/product-item/ProductItem.js
@@ -1,36 +1,15 @@
-import React, { Component }  from 'react';
-import './ProductItem.css'
+import React, { Component } from "react";
+
+import "./ProductItem.css";
 
 class ProductItem extends Component {
-    
-    render(){
-      return(  
-        <div className="ProductItem">
-            <div class="item">
-        
-                <div class="name-quantity">
-                    <span>Product Name</span>
-                    <span>Quantity: X</span>
-                </div>
-        
-                <div class="total-price">$549</div>
-
-                <div class="quantity">
-                    <button class="plus-btn" type="button" name="button">
-                        <img src="plus.svg" alt="" />
-                    </button>
-                    
-                    <button class="minus-btn" type="button" name="button">
-                        <img src="minus.svg" alt="" />
-                    </button>
-                </div>
-        
-                
-            </div>
-        </div>
-        )
-    }
-  
+  render() {
+    return (
+      <div className="ProductItem">
+        <div class="item"></div>
+      </div>
+    );
   }
-  
-  export default ProductItem;
+}
+
+export default ProductItem;
